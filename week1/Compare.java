54e9141532727e8658e4e35ff9a02e9f04378ba4
semil <semiloreomotade@gmail.com>
2026-03-17 04:33:42 +0100

    add: Comparing Integers

--- week1/Compare.java
+++ week1/Compare.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Compare{
   public static void main(String[] args){
@@ -6,15 +7,16 @@ public class Compare{
 
     System.out.println("Input your number");
     int num = input.nextInt();
+    double power = Math.pow(num, 2);
 
     if (num > 100) System.out.printf("%d is greater than 100 %n", num);
-    if (num**2 > 100) System.out.printf("%d squared is greater than 100 %n", num**2);
+    if (power > 100.0) System.out.printf("%f squared is greater than 100 %n", power);
 
     if (num == 100) System.out.printf("%d is equal to 100 %n", num);
-    if (num**2 == 100) System.out.printf("%d squared is equal to 100 %n", num**2);
+    if (power == 100.0) System.out.printf("%f squared is equal to 100 %n", power);
 
 
     if (num < 100) System.out.printf("%d is less than 100 %n", num);
-    if (num**2 < 100) System.out.printf("%d squared is less than 100 %n", num**2);
+    if (power < 100.0) System.out.printf("%f squared is less than 100 %n", power);
   }
 }
