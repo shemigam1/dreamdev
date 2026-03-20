2911751f92febfd566f1524a5b061bdbf94939cb
semil <semiloreomotade@gmail.com>
2026-03-20 19:32:07 +0100

    fix: isPrimeNumber

--- src/snacks/Kata.java
+++ src/snacks/Kata.java
@@ -9,7 +9,7 @@ public class Kata {
     public static boolean isPrimeNumber(int number){
         if (number < 2) return false;
 
-        for (int i = 0; i < Math.sqrt(number); i++){
+        for (int i = 2; i < Math.sqrt(number); i++){
             if (number % i == 0) return false;
         }
         return true;
