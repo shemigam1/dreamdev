f0c9580ca828724afb0fe8e1fa2b98d432828c1f
semil <semiloreomotade@gmail.com>
2026-03-20 19:44:49 +0100

    add: factorsOf

--- src/snacks/Kata.java
+++ src/snacks/Kata.java
@@ -18,4 +18,17 @@ public class Kata {
     public static int subtract(int num1, int num2){
         return Math.abs(num1 - num2);
     }
+
+    public static float divide(int num1, int num2){
+        if (num2 == 0) return 0;
+        return (float) num1 / num2;
+    }
+
+    public static int factorOf(int number){
+        int factors = 0;
+        for (int i = 1; i <= number; i++){
+            if (number % i == 0) factors++;
+        }
+        return factors;
+    }
 }
