14f03bd7507000a99dab13bdb1b747290f6dfa2d
semil <semiloreomotade@gmail.com>
2026-03-20 19:22:10 +0100

    fix: static

--- test/snacks/BackToSenderTest.java
+++ test/snacks/BackToSenderTest.java
@@ -7,40 +7,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BackToSenderTest {
-    private BackToSender payroll;
-    @BeforeEach
-    public void setup(){
-        payroll = new BackToSender();
-    }
-
     @Test
     @DisplayName("Function should return an integer")
     public void isInt(){
-        assertInstanceOf(Integer.class, payroll.payout(5));
+        assertInstanceOf(Integer.class, BackToSender.payout(5));
     }
 
     @Test
     @DisplayName("Function should return correct value of < 50")
     public void lessThanFifty(){
-        assertEquals(12840, payroll.payout(49));
+        assertEquals(12840, BackToSender.payout(49));
     }
 
     @Test
     @DisplayName("Function should return correct value of < 60 >= 50")
     public void lessThanSixty(){
-        assertEquals(16800, payroll.payout(59));
+        assertEquals(16800, BackToSender.payout(59));
     }
 
     @Test
     @DisplayName("Function should return correct value of < 70 >= 60")
     public void lessThanSeventy(){
-        assertEquals(22250, payroll.payout(69));
+        assertEquals(22250, BackToSender.payout(69));
     }
 
     @Test
     @DisplayName("Function should return correct value of >= 70")
     public void moreThanSeventy(){
-        assertEquals(40500, payroll.payout(71));
+        assertEquals(40500, BackToSender.payout(71));
     }
 
 
