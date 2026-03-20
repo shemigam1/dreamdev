5e8d3b9d23c1f8809db34d8620fb4e48262946d3
semil <semiloreomotade@gmail.com>
2026-03-20 19:20:00 +0100

    add: payout function

--- src/snacks/BackToSender.java
+++ src/snacks/BackToSender.java
@@ -2,8 +2,19 @@ package snacks;
 
 public class BackToSender {
 
-    public int payout(int numOfDeliveries){
+    public static int payout(int numOfDeliveries){
+        int basePay = 5000;
+        int commissions = calculateCommission(numOfDeliveries);
+        return basePay + commissions;
+    }
 
+    private static int calculateCommission(int numOfDeliveries){
+        int rate = 160;
+        if (numOfDeliveries >= 50) rate = 200;
+        if (numOfDeliveries >= 60) rate = 250;
+        if (numOfDeliveries >= 70) rate = 500;
+
+        return rate * numOfDeliveries;
     }
 
 
