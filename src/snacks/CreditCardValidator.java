5e214f0ff1e6604e105013e1359f50acbce7f7f4
semil <semiloreomotade@gmail.com>
2026-03-21 17:19:19 +0100

    add: credit card validator init

---
+++ src/snacks/CreditCardValidator.java
@@ -0,0 +1,14 @@
+package snacks;
+
+public class CreditCardValidator {
+    public static void main(String... args){
+
+    }
+
+    public String checkType(int creditCardNumber){
+
+    }
+
+    public boolean validate()
+
+}
