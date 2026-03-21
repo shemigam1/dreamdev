3163c8560c74c8f147f56d20511cce2762fdd332
semil <semiloreomotade@gmail.com>
2026-03-21 15:58:43 +0100

    add: max value, min value, sum of

--- src/snacks/ArrayKata.java
+++ src/snacks/ArrayKata.java
@@ -2,6 +2,26 @@ package snacks;
 
 public class ArrayKata {
     public static int maximumIn(int[] arrayOfIntegers){
+        int max = Integer.MIN_VALUE;
+        for (int num : arrayOfIntegers){
+            if (num > max) max = num;
+        }
+        return max;
+    }
 
+    public static int minimumIn(int[] arrayOfIntegers){
+        int min = Integer.MAX_VALUE;
+        for (int num : arrayOfIntegers){
+            if (num < min) min = num;
+        }
+        return min;
+    }
+
+    public static int sumOf(int[] arrayOfIntegers){
+        int sum = 0;
+        for (int num : arrayOfIntegers){
+            sum += num;
+        }
+        return sum;
     }
 }
