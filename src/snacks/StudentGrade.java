1cf6f7864d3678fe5ac74a72be354b7ad8d05101
semil <semiloreomotade@gmail.com>
2026-03-21 22:13:36 +0100

    add: take in scores

--- src/snacks/StudentGrade.java
+++ src/snacks/StudentGrade.java
@@ -13,6 +13,17 @@ public class StudentGrade {
         int totalSubjects = scanner.nextInt();
 
         int[][] classRoom = new int[totalStudents][totalSubjects];
+        for (int i = 0; i < totalStudents; i++){
+            for (int j = 0; j < totalSubjects; j++){
+                System.out.printf("Entering score for student %d%n", i);
+                System.out.printf("Entering score for subject %d%n", j);
+                int score = scanner.nextInt();
+                System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>>>");
+                System.out.println("Saved successfully");
+                classRoom[i][j] = score;
+            }
+        }
 
+        System.out.println(classRoom);
     }
 }
