6190cd1e0f073d71c9c5fa3df4badad86cfe4d36
semil <semiloreomotade@gmail.com>
2026-03-21 18:17:11 +0100

    add: credit card validator v.0

--- src/snacks/CreditCardValidator.java
+++ src/snacks/CreditCardValidator.java
@@ -1,14 +1,59 @@
 package snacks;
 
+import java.util.Scanner;
+
 public class CreditCardValidator {
     public static void main(String... args){
+        System.out.println("Hello, kindly enter details to verify");
+        Scanner scanner = new Scanner(System.in);
 
+        int cardNumber = scanner.nextInt();
+        String cardType = checkType(cardNumber);
+        String isValid = validate(cardNumber);
+        int cardDigitLength = String.valueOf(cardNumber).length();
+        if (!(cardDigitLength >= 13 && cardDigitLength <= 16)) {
+            isValid = "Invalid";
+        }
+
+        System.out.println("********************************");
+        System.out.printf("***Credit Card Type: %s%n", cardType);
+        System.out.printf("***Credit Card Number: %d%n", cardNumber);
+        System.out.printf("***Credit Card Digit Length: %d%n", cardDigitLength);
+        System.out.printf("***Credit Card Validity Status: %s%n", isValid);
     }
 
-    public String checkType(int creditCardNumber){
+    public static String checkType(int cardNumber){
+        String cardString = String.valueOf(cardNumber);
 
+        String cardType = "unknown";
+        if (cardString.charAt(0) == '4') cardType = "Visa Card";
+        if (cardString.charAt(0) == '5') cardType = "MasterCard";
+        if (cardString.charAt(0) == '6') cardType = "Discover Card";
+        if (cardString.charAt(0) == '3' && cardString.charAt(1) == '7') cardType = "American Express Card";
+        return cardType;
     }
 
-    public boolean validate()
+    public static String validate(int cardNumber){
+        String cardString = String.valueOf(cardNumber);
+        int even = 0;
+        int odd = 0;
+
+        for (int i = cardString.length() - 2; i >=0  ; i -= 2){
+            int num = (int) cardString.charAt(i) * 2;
+            if (num > 9){
+                String numString = String.valueOf(num);
+                even += (int) numString.charAt(0) + (int) numString.charAt(1);
+                continue;
+            }
+            even += num;
+        }
+
+        for (int j = cardString.length() - 1; j >=0  ; j -= 2){
+            odd += (int) cardString.charAt(j);
+        }
+        boolean isValid = (odd + even) % 10 == 0;
+        if (isValid) return "Valid";
+        return "Invalid";
+    }
 
 }
