8c4b6b672053a8244182c74ab8ca52a66b19dcfc
semil <semiloreomotade@gmail.com>
2026-03-21 21:56:44 +0100

    add: student grade v0

--- src/snacks/StudentGrade.java
+++ src/snacks/StudentGrade.java
@@ -1,5 +1,18 @@
 package snacks;
 
+import java.util.Scanner;
+
 public class StudentGrade {
-    public static void main(String... args){}
+    public static void main(String... args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many students do you have?");
+        int totalStudents = scanner.nextInt();
+
+        System.out.println("How many subject do they offer");
+        int totalSubjects = scanner.nextInt();
+
+        int[][] classRoom = new int[totalStudents][totalSubjects];
+
+    }
 }
