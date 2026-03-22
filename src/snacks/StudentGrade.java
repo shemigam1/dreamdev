58539b009ee056c3f5495db51969da3b4ba67a31
semil <semiloreomotade@gmail.com>
2026-03-22 20:09:29 +0100

    add: subject sumary, class summary

--- src/snacks/StudentGrade.java
+++ src/snacks/StudentGrade.java
@@ -101,9 +101,56 @@ public class StudentGrade {
             """, j + 1, highest[0], highest[1], lowest[0], lowest[1], total, average, passes, fails);
 
             body += scoreSummary;
+
 //            System.out.println(scoreSummary);
         }
+        body += "\n";
 
+        int[] hardest = hardestSubject(classRoom, totalSubjects);
+        int[] easiest = easiestSubject(classRoom, totalSubjects);
+        int[] highestOverall = highestOverallScore(classRoom);
+        int[] lowestOverall = lowestOverallScore(classRoom);
+        int[] best = bestGraduatingStudent(totalScores);
+        int[] worst = worstGraduatingStudent(totalScores);
+
+        String subjectSummary = String.format("""
+        The hardest subject is Subject %d with %d failures
+        The easiest subject is Subject %d with %d passes
+        The overall highest score is scored by Student %d in Subject %d scoring %d
+        The overall lowest score is scored by Student %d in Subject %d scoring %d
+        The best graduating student is Student %d with a total score of %d
+        The worst graduating student is Student %d with a total score of %d
+        ===============================================================
+        """,
+                hardest[0], hardest[1],
+                easiest[0], easiest[1],
+                highestOverall[0], highestOverall[1], highestOverall[2],
+                lowestOverall[0], lowestOverall[1], lowestOverall[2],
+                best[0], best[1],
+                worst[0], worst[1]);
+
+        body += subjectSummary;
+
+        String classSummary = String.format("""
+        CLASS SUMMARY
+        ===============================================================
+        Best Graduating student is: Student %d scoring %d
+        ===============================================================
+        
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Worst Graduating student is: Student %d scoring %d
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+        ===============================================================
+        Class total score is: %d
+        Class Average score is: %.2f
+        ===============================================================
+        """,
+                best[0], best[1],
+                worst[0], worst[1],
+                classTotalScore(totalScores),
+                classAverageScore(totalScores));
+        body += classSummary;
         System.out.println(body);
     }
 
@@ -154,4 +201,118 @@ public class StudentGrade {
         }
         return total;
     }
+
+    public static int[] hardestSubject(int[][] classRoom, int totalSubjects){
+        int hardestIdx = 0;
+        int mostFails = 0;
+
+        for (int j = 0; j < totalSubjects; j++){
+            int[] subjectColumn = new int[classRoom.length];
+            for (int i = 0; i < classRoom.length; i++){
+                subjectColumn[i] = classRoom[i][j];
+            }
+            int fails = totalFailScores(subjectColumn);
+            if (fails > mostFails){
+                mostFails = fails;
+                hardestIdx = j;
+            }
+        }
+        return new int[]{hardestIdx + 1, mostFails};
+    }
+
+    public static int[] easiestSubject(int[][] classRoom, int totalSubjects){
+        int easiestIdx = 0;
+        int mostPasses = 0;
+
+        for (int j = 0; j < totalSubjects; j++){
+            int[] subjectColumn = new int[classRoom.length];
+            for (int i = 0; i < classRoom.length; i++){
+                subjectColumn[i] = classRoom[i][j];
+            }
+            int passes = totalPassScores(subjectColumn);
+            if (passes > mostPasses){
+                mostPasses = passes;
+                easiestIdx = j;
+            }
+        }
+        return new int[]{easiestIdx + 1, mostPasses};
+    }
+
+    public static int[] highestOverallScore(int[][] classRoom){
+        int studentIdx = 0;
+        int subjectIdx = 0;
+        int highestScore = 0;
+
+        for (int i = 0; i < classRoom.length; i++){
+            for (int j = 0; j < classRoom[i].length; j++){
+                if (classRoom[i][j] > highestScore){
+                    highestScore = classRoom[i][j];
+                    studentIdx = i;
+                    subjectIdx = j;
+                }
+            }
+        }
+        return new int[]{studentIdx + 1, subjectIdx + 1, highestScore};
+    }
+
+    public static int[] lowestOverallScore(int[][] classRoom){
+        int studentIdx = 0;
+        int subjectIdx = 0;
+        int lowestScore = Integer.MAX_VALUE;
+
+        for (int i = 0; i < classRoom.length; i++){
+            for (int j = 0; j < classRoom[i].length; j++){
+                if (classRoom[i][j] < lowestScore){
+                    lowestScore = classRoom[i][j];
+                    studentIdx = i;
+                    subjectIdx = j;
+                }
+            }
+        }
+        return new int[]{studentIdx + 1, subjectIdx + 1, lowestScore};
+    }
+
+    public static int[] bestGraduatingStudent(int[] totalScores){
+        int studentIdx = 0;
+        int highestTotal = 0;
+
+        for (int i = 0; i < totalScores.length; i++){
+            if (totalScores[i] > highestTotal){
+                highestTotal = totalScores[i];
+                studentIdx = i;
+            }
+        }
+        return new int[]{studentIdx + 1, highestTotal};
+    }
+
+    public static int[] worstGraduatingStudent(int[] totalScores){
+        int studentIdx = 0;
+        int lowestTotal = Integer.MAX_VALUE;
+
+        for (int i = 0; i < totalScores.length; i++){
+            if (totalScores[i] < lowestTotal){
+                lowestTotal = totalScores[i];
+                studentIdx = i;
+            }
+        }
+        return new int[]{studentIdx + 1, lowestTotal};
+    }
+
+    public static int classTotalScore(int[] totalScores){
+        int total = 0;
+        for (int score: totalScores){
+            total += score;
+        }
+        return total;
+    }
+
+    public static double classAverageScore(int[] totalScores){
+        int total = 0;
+        for (int score: totalScores){
+            total += score;
+        }
+        return (double) total / totalScores.length;
+    }
+
+
 }
