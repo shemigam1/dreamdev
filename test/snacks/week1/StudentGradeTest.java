f8660e1ba54e9a4d862bf90723cd2b31836ee9b2
semil <semiloreomotade@gmail.com>
2026-03-22 23:00:05 +0100

    add: student grade test v1

--- test/snacks/week1/StudentGradeTest.java
+++ test/snacks/week1/StudentGradeTest.java
@@ -17,4 +17,22 @@ public class StudentGradeTest {
     public void highestScoringStudentBasic(){
         assertArrayEquals(new int[]{1, 90}, StudentGrade.highestScoringStudent(new int[]{90, 60, 30}));
     }
+
+    @Test
+    @DisplayName("Highest scoring student last position, expect student 3 with 95")
+    public void highestScoringStudentLast(){
+        assertArrayEquals(new int[]{3, 95}, StudentGrade.highestScoringStudent(new int[]{60, 70, 95}));
+    }
+
+    @Test
+    @DisplayName("Lowest scoring student in subject, expect student 3 with 30")
+    public void lowestScoringStudentBasic(){
+        assertArrayEquals(new int[]{3, 30}, StudentGrade.lowestScoringStudent(new int[]{90, 60, 30}));
+    }
+
+    @Test
+    @DisplayName("Lowest scoring student first position, expect student 1 with 30")
+    public void lowestScoringStudentFirst(){
+        assertArrayEquals(new int[]{1, 30}, StudentGrade.lowestScoringStudent(new int[]{30, 60, 95}));
+    }
 }
