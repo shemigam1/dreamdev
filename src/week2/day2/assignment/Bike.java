5f7cf630e55c7d670223a276261f441a86fffa2a
semil <semiloreomotade@gmail.com>
2026-03-25 07:45:41 +0100

    day 2 assignment bike: accelerate and decelerate

--- src/week2/day2/assignment/Bike.java
+++ src/week2/day2/assignment/Bike.java
@@ -3,11 +3,77 @@ package week2.day2.assignment;
 public class Bike {
     public String power = "off";
     public int speed = 0;
+    private int limit = 85;
     public void turnOn() {
         this.power = "on";
     }
 
     public void turnOff() {
         this.power = "off";
+    }
+
+    public void accelerate(int throttle) {
+        if (this.power.equals("off")) return;
+        if (this.speed >= limit) {
+            System.out.println("Speed limit reached");
+            return;
+        };
+
+        int acc;
+        if (this.speed > 40) {
+            System.out.println("Entering Gear 4");
+            acc = gearFour(throttle);
+        } else if (this.speed > 30) {
+            System.out.println("Entering Gear 3");
+            acc = gearThree(throttle);
+        } else if (this.speed > 20) {
+            System.out.println("Entering Gear 2");
+            acc = gearTwo(throttle);
+        } else {
+            acc = throttle;
+        }
+        this.speed = this.speed + acc;
+        System.out.printf("Speed: %d%n", this.speed);
+
+        if (this.speed >= limit){
+            this.speed = limit;
+        }
+    }
+
+    public int gearTwo(int throttle){
+        return throttle * 2;
+    }
+
+    public int gearThree(int throttle){
+        return throttle * 3;
+    }
+
+    public int gearFour(int throttle){
+        return throttle * 4;
+    }
+
+    public void decelerate(int throttle) {
+        if (this.power.equals("off")) return;
+        if (this.speed <= 0) {
+            System.out.println("Bike is not moving");
+            return;
+        }
+
+        int acc;
+        if (this.speed > 40) {
+            System.out.println("Leaving Gear 4");
+            acc = gearFour(throttle);
+        } else if (this.speed > 30) {
+            System.out.println("Leaving Gear 3");
+            acc = gearThree(throttle);
+        } else if (this.speed > 20) {
+            System.out.println("Leaving Gear 2");
+            acc = gearTwo(throttle);
+        } else {
+            acc = throttle;
+        }
+
+        this.speed = Math.max(this.speed - acc, 0);
+        System.out.printf("Speed: %d%n", this.speed);
     }
 }
--- test/week2/day2/BikeTest.java
+++ test/week2/day2/BikeTest.java
@@ -1,5 +1,6 @@
 package week2.day2;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import week2.day2.assignment.Bike;
@@ -7,19 +8,155 @@ import week2.day2.assignment.Bike;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BikeTest {
+    private Bike bike;
+
+    @BeforeEach
+    public void setup(){
+        bike = new Bike();
+    }
+
     @Test
-    @DisplayName("Bike test on")
-    public void biketestOnWhenOff(){
-        Bike bike = new Bike();
+    @DisplayName("Bike test on when off")
+    public void bikeTestOnWhenOff(){
+
         assertEquals("off", bike.power);
         bike.turnOn();
         assertEquals("on", bike.power);
     }
 
-    public void biketestOffWhenOn(){
-        Bike bike = new Bike();
+    @Test
+    @DisplayName("Bike test off when on")
+    public void bikeTestOffWhenOn(){
+        bike.turnOn();
         assertEquals("on", bike.power);
         bike.turnOff();
         assertEquals("off", bike.power);
+    }
+
+    @Test
+    @DisplayName("Bike accelerates when on")
+    public void bikeTestAccelerateWhenOn(){
+        bike.turnOn();
+        assertEquals("on", bike.power);
+        bike.accelerate(2);
+        assertEquals(2, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike accelerates when on to gear 2")
+    public void bikeTestAccelerateGear2(){
+        bike.turnOn();
+        assertEquals("on", bike.power);
+        bike.accelerate(25);
+        assertEquals(25, bike.speed);
+
+        bike.accelerate(2);
+        assertEquals(29, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike accelerates when on to gear 3")
+    public void bikeTestAccelerateGear3(){
+        bike.turnOn();
+        assertEquals("on", bike.power);
+        bike.accelerate(32);
+        assertEquals(32, bike.speed);
+
+        bike.accelerate(2);
+        assertEquals(38, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike accelerates when on from gear 2 to gear 3")
+    public void bikeTestAccelerateSwitchGears(){
+        bike.turnOn();
+        assertEquals("on", bike.power);
+        bike.accelerate(38);
+        assertEquals(38, bike.speed);
+
+        bike.accelerate(2);
+        assertEquals(38+3+3, bike.speed);
+
+        bike.accelerate(2);
+        assertEquals(38+3+3+4+4, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike decelerates when on")
+    public void bikeTestDecelerateWhenOn(){
+        bike.turnOn();
+        bike.accelerate(10);
+        assertEquals(10, bike.speed);
+
+        bike.decelerate(2);
+        assertEquals(8, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike decelerates from gear 2")
+    public void bikeTestDecelerateGear2(){
+        bike.turnOn();
+        bike.accelerate(25);
+        assertEquals(25, bike.speed);
+
+        bike.decelerate(2);
+        assertEquals(21, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike decelerates from gear 3")
+    public void bikeTestDecelerateGear3(){
+        bike.turnOn();
+        bike.accelerate(32);
+        assertEquals(32, bike.speed);
+
+        bike.decelerate(2);
+        assertEquals(26, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike decelerates from gear 4")
+    public void bikeTestDecelerateGear4(){
+        bike.turnOn();
+        bike.accelerate(45);
+        assertEquals(45, bike.speed);
+
+        bike.decelerate(2);
+        assertEquals(37, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike decelerates switching gears")
+    public void bikeTestDecelerateSwitchGears(){
+        bike.turnOn();
+        bike.accelerate(45);
+        assertEquals(45, bike.speed);
+
+        bike.decelerate(2);
+        assertEquals(45-4-4, bike.speed);
+
+        bike.decelerate(2);
+        assertEquals(45-4-4-3-3, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike decelerates to 0, expect 0 not negative")
+    public void bikeTestDecelerateFloor(){
+        bike.turnOn();
+        bike.accelerate(2);
+        assertEquals(2, bike.speed);
+
+        bike.decelerate(10);
+        assertEquals(0, bike.speed);
+    }
+
+    @Test
+    @DisplayName("Bike decelerate when not moving, no change")
+    public void bikeTestDecelerateWhenStopped(){
+        bike.turnOn();
+        assertEquals(0, bike.speed);
+
+        bike.decelerate(2);
+        assertEquals(0, bike.speed);
     }
 }
