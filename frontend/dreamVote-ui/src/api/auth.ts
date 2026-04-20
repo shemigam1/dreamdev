295580b037fc8cdc51607983d4f5b1c232164309
semil <semiloreomotade@gmail.com>
2026-04-21 00:21:16 +0100

    feat: setup auth api

---
+++ frontend/dreamVote-ui/src/api/auth.ts
@@ -0,0 +1,32 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (body) => ({
+        url: "/voters/",
+        method: "POST",
+        body,
+      }),
+    }),
+    login: builder.mutation({
+      query: (body) => ({
+        url: "/voters/login",
+        method: "POST",
+        body,
+      }),
+    }),
+
+    logout: builder.mutation({
+      query: () => ({
+        url: "/voters/logout",
+        method: "POST",
+      }),
+    }),
+  }),
+});
